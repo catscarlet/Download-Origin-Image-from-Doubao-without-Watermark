36d88f756941d4de1c62e4410a39072c7bd86ef9
catscarlet <i@catscarlet.com>
2026-03-21 23:37:02 +0800

    New Feature: Download preview videos.

--- Download-Origin-Image-from-Doubao-without-Watermark.user.js
+++ Download-Origin-Image-from-Doubao-without-Watermark.user.js
@@ -46,8 +46,10 @@ const customPostfixName = '';
                         }
 
                         let images = [];
+                        let videos = [];
                         //const imagesOldVersion = document.querySelectorAll('img.preview-img-IlQuCi.img-bg-fz6Iim');
                         const imagesNewVersion = document.querySelectorAll('div.img-preview-container-aIXnUl');
+                        const videosNewVersion = document.querySelectorAll('div.video-canvas-panel-content-fbsXkh');
 
                         /*
                         for (const imageValue of imagesOldVersion.values()) {
@@ -59,17 +61,29 @@ const customPostfixName = '';
                             images.push(imageValue);
                         }
 
-                        if (images.length == 0) {
+                        for (const videoValue of videosNewVersion.values()) {
+                            videos.push(videoValue);
+                        }
+
+                        if (images.length == 0 && videos.length == 0) {
                             return false;
                         }
 
                         images.forEach((image) => {
-
                             if (!image.parentNode.querySelector('.imagelink-nowatermark-527890')) {
                                 const link = createImageDownloadButton();
                                 image.parentNode.appendChild(link);
                             } else {
-                                //console.log('added, skip.');
+                                //console.log('image.parentNode.appendChild added, skip.');
+                            }
+                        });
+
+                        videos.forEach((video) => {
+                            if (!video.parentNode.querySelector('.videolink-527890')) {
+                                const link = createVideoDownloadButton();
+                                video.parentNode.appendChild(link);
+                            } else {
+                               //console.log('video.parentNode.appendChild added, skip.');
                             }
                         });
                     }
@@ -136,6 +150,54 @@ function createImageDownloadButton() {
     return link;
 }
 
+function createVideoDownloadButton() {
+    const link = document.createElement('a');
+
+    link.textContent = '点击下载以「会话名-会话ID-下载时间」为文件名的预览视频文件';
+    link.style.whiteSpace = 'break-spaces';
+
+    link.classList.add('videolink-nowatermark-527890');
+
+    link.style.position = 'absolute';
+    link.style.backgroundColor = '#007BFF';
+    link.style.color = 'white';
+    link.style.padding = '7px 14px';
+    link.style.border = 'none';
+    link.style.borderRadius = '5px';
+
+    link.style.zIndex = 1;
+    link.style.textDecoration = 'none';
+    link.style.opacity = '0.8';
+
+    const x = 0;
+    const y = 0;
+
+    link.style.left = x + 'px';
+    link.style.top = y + 'px';
+
+    link.addEventListener('mouseover', function() {
+        if (this.style.cursor == 'not-allowed') {
+            return;
+        }
+        this.style.backgroundColor = '#0056b3';
+        this.style.cursor = 'pointer';
+    });
+
+    link.addEventListener('mouseout', function() {
+        if (this.style.cursor == 'not-allowed') {
+            return;
+        }
+        this.style.backgroundColor = '#007BFF';
+        this.style.cursor = '';
+    });
+
+    link.addEventListener('click', async () => {
+        getCrossVideo(link);
+    });
+
+    return link;
+}
+
 async function getCrossOriginImage(link) {
 
     const btnOriginStyle = {};
@@ -185,6 +247,55 @@ async function getCrossOriginImage(link) {
 
 }
 
+async function getCrossVideo(link) {
+
+    const btnOriginStyle = {};
+    btnOriginStyle.cursor = link.style.cursor;
+    btnOriginStyle.backgroundColor = link.style.backgroundColor;
+    link.style.cursor = 'not-allowed';
+    link.style.backgroundColor = 'grey';
+
+    const currentTitle = document.title.replace('- 豆包', '').trim();
+    const chatID = document.location.pathname.replace('/chat/', '').trim();
+    const timeStr = getYmdHMS();
+
+    const videoNodelist = link.parentNode.querySelectorAll('video');
+    const videoUrl = Array.from(videoNodelist).find((element) => element.tagName.toLowerCase() == 'video').src;
+
+    let videoName = currentTitle + '-' + chatID + '-' + timeStr;
+    if (customPostfixName) {
+        videoName = videoName + '-' + customPostfixName;
+    }
+    videoName = videoName + '.mp4';
+
+    try {
+        const response = await fetch(videoUrl, {mode: 'cors'});
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = videoName;
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        setTimeout(() => {
+            a.click();
+        }, 10);
+        setTimeout(() => {
+            URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+            link.style.cursor = btnOriginStyle.cursor;
+            link.style.backgroundColor = btnOriginStyle.backgroundColor;
+        }, 1000);
+
+    } catch (error) {
+        console.error('视频加载失败，请确保图片服务器开启了 CORS 支持。');
+        alert('视频加载失败，请确保视频服务器开启了 CORS 支持。');
+        link.style.cursor = btnOriginStyle.cursor;
+        link.style.backgroundColor = btnOriginStyle.backgroundColor;
+    }
+
+}
+
 function getYmdHMS() {
     const date = new Date();
     const Y = date.getFullYear();
@@ -208,7 +319,6 @@ function addAndChangeStyles() {
         const cssRule = `.left-top-w75xeC {
             justify-content: center !important;
         }`;
-
 
         style.appendChild(document.createTextNode(cssRule));
 
